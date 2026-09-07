b047c116b63e91e8ed56535c321b35777563051d
Joseph DELNORD <joseph.delnord@supdevinci-edu.fr>
2026-09-07 10:40:45 +0200

    fix: ruff import ordering

--- src/evaluate.py
+++ src/evaluate.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 import joblib
 import matplotlib.pyplot as plt
+import mlflow
 import pandas as pd
+
 from sklearn.metrics import (
     ConfusionMatrixDisplay,
     PrecisionRecallDisplay,
@@ -15,7 +17,6 @@ from sklearn.metrics import (
 )
 from sklearn.model_selection import train_test_split
 
-import mlflow
 from src.utils import (
     load_config,
     load_data,
