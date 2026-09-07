d373bb588b21ff7ecca02dd9709c5e28e74a40d0
Joseph DELNORD <joseph.delnord@supdevinci-edu.fr>
2026-09-07 14:17:07 +0200

    update docker compose, pipeline

--- src/mlflow_logging.py
+++ src/mlflow_logging.py
@@ -1,17 +1,17 @@
 # src/mlflow_logging.py
+import mlflow
 import os
 from pathlib import Path
-
-import mlflow
-
 
 def log_experiment():
     # Définir le nom de l'expérience
     experiment_name = "mlops-project-experiment"
-    mlflow.set_experiment(experiment_name)
+
+    # Créer une expérience manuellement
+    experiment_id = mlflow.create_experiment(experiment_name)
 
     # Enregistrer les métriques, paramètres et modèles
-    with mlflow.start_run():
+    with mlflow.start_run(experiment_id=experiment_id):
         # Exemple de métriques
         mlflow.log_metric("accuracy", 0.95)
         mlflow.log_metric("precision", 0.92)
@@ -27,7 +27,3 @@ def log_experiment():
             mlflow.sklearn.log_model(model_path, "random-forest-model")
         else:
             print(f"Model file not found: {model_path}")
-
-
-if __name__ == "__main__":
-    log_experiment()
